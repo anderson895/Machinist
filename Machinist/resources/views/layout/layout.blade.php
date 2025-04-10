f6828d114c5755748242a247bef61ceb2c067775
JARP <105678913+anderson895@users.noreply.github.com>
2025-04-10 16:47:12 +0800

    first commit

---
+++ Machinist/resources/views/layout/layout.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body>
+
+    @include('layout.nav')
+    @yield('content')
+    @include('layout.footer')
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+</body>
+</html>
---
+++ Machinist/resources/views/welcome.blade.php
@@ -0,0 +1,5 @@
+@extends('layout.layout')
+
+@section('content')
+<button class="btn btn-primary">Test</button>
+@endsection
